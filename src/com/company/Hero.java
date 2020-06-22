0335dbab9bb271d2606f51a7597fe023566f248d
Soliardi-Chyngyz <wxwaren@gmail.com>
2020-06-22 15:17:56 +0600

    Final part

--- src/com/company/Hero.java
+++ src/com/company/Hero.java
@@ -1,21 +1,25 @@
 package com.company;
 
 public class Hero {
+    private String name;
     private int health;
     private int damage;
     private String superPower;
 
-    public Hero(int hlth, int dam, String supPower){
+    public Hero(String name, int hlth, int dam, String supPower){
+        this.name = name;
         this.health = hlth;
         this.damage = dam;
         this.superPower = supPower;
     }
 
-    public Hero(int hlth, String supPower){
+    public Hero(String name, int hlth, String supPower){
+        this.name = name;
         this.health = hlth;
         this.superPower = supPower;
     }
 
+    public String getName() {return name;}
     public int getHealth() {return health;}
     public int getDamage() {return damage;}
     public String getSuperPower() {return superPower;}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,39 +1,36 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
         Boss bossDefeanse = new Boss();
 
         Boss boss = new Boss(1000, 100, "Magic");
+
         System.out.println(boss.getHealth() + " " + boss.getDamage() + " super power is " + boss.getDefense());
 
         createHeroes();
 
-
-
     }
 
-    public static int createHeroes(){
+    public static com.company.Hero[] createHeroes(){
         Hero[] heros = new Hero[3];
 
-        Hero strikoza = new Hero (400, "fly");
+        Hero strikoza = new Hero ("strikoza", 400, "fly");
         heros[0] = strikoza;
-        int strikozaHealth = strikoza.getHealth();
-        System.out.println("strikoza " + strikoza.getHealth() + " " + strikoza.getSuperPower());
-//        return strikoza;
+//        int strikozaHealth = strikoza.getHealth();
+//        System.out.println("strikoza " + strikoza.getHealth() + " " + strikoza.getSuperPower());
 
-        Hero borsok = new Hero(500,40,"ожирение");
+        Hero borsok = new Hero("borsok",500,40,"ожирение");
         heros[1] = borsok;
-        System.out.println("borsok " + borsok.getHealth() + " " + borsok.getSuperPower());
+//        System.out.println("borsok " + borsok.getHealth() + " " + borsok.getSuperPower());
 
-        Hero chypalak = new Hero(400, 30, "разгибается");
+        Hero chypalak = new Hero("chypalak", 400, 30, "разгибается");
         heros[2] = chypalak;
-        System.out.println("chypalak " + " " + chypalak.getHealth() + " " + chypalak.getDamage() + " " + chypalak.getSuperPower());
+//        System.out.println("chypalak " + " " + chypalak.getHealth() + " " + chypalak.getDamage() + " " + chypalak.getSuperPower());
 
-        for (int i = 0; i < heros.length; i++) {
-//            System.out.println("i " + i.getHealth() +);
+        for (int i = 0; i < 3; i++) {
+            System.out.print(heros[i].getName() + " - " + heros[i].getHealth() + " " + heros[i].getDamage() + " " + heros[i].getSuperPower() + " | ");
         }
-        return createHeroes();
+        return heros;
     }
 }
